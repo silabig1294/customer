8320ce54306de1b94b8b4ac23ef4fb26db1405c9
sila siangchin <sila.se@inet.co.th>
2023-05-23 10:48:46 +0700

    dev first backend test

--- main.go
+++ main.go
@@ -1,24 +1,10 @@
 package main
 
-import (
+import(
 	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/middleware/cors"
-	db "github.com/silabig1294/customer/database"
-	"github.com/silabig1294/customer/routes"
 )
 
-func main() {
-	db.Connect()
 
-	app := fiber.New()
-	app.Use(cors.New(
-		cors.Config{
-		 	AllowCredentials: true,
-		},
-		))
-	routes.Setup(app)
-	// app.Get("/",func(c *fiber.Ctx)error{
-	// 	return c.SendString("Hello Work!!!")
-	// })
-	app.Listen(":8050")
+func main() {
+	
 }
